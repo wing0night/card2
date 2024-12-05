206b1b2cd749d09f3334e1c74372887275afc5c7
wing0night <119767073+wing0night@users.noreply.github.com>
2024-12-06 03:46:06 +0800

    更新 BirthdayCard.tsx

--- src/components/BirthdayCard.tsx
+++ src/components/BirthdayCard.tsx
@@ -5,7 +5,7 @@ import RandomImages from './RandomImages'
 
 const BirthdayCard = () => {
   return (
-    <div className="relative w-full max-w-8xl aspect-video overflow-hidden">
+    <div className="relative w-full max-w-20xl aspect-video overflow-hidden">
       <ConfettiEffect />
       <CursorImageEffect />
       <RandomImages />
@@ -14,7 +14,7 @@ const BirthdayCard = () => {
         <span className="text-gray-500">Add your background image here</span>
       </div> */}
       
-      <div className="relative z-40 h-full flex flex-col items-center justify-center p-8">
+      <div className="relative z-20 h-full flex flex-col items-center justify-center p-4">
         <h1 className="text-6xl font-bold text-center mb-8 birthday-text">
           Happy Birthday
         </h1>
