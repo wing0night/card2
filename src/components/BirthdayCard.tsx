751c573eb96e86786775f92f49ed3d25fd764149
NIGHT <3210103847@zju.edu.cn>
2024-12-06 10:50:04 +0800

    Update BirthdayCard.tsx

--- src/components/BirthdayCard.tsx
+++ src/components/BirthdayCard.tsx
@@ -3,26 +3,23 @@ import ConfettiEffect from './ConfettiEffect'
 import CursorImageEffect from './CursorImageEffect'
 import RandomImages from './RandomImages'
 
+// birthday_card/src/components/BirthdayCard.tsx
 const BirthdayCard = () => {
   return (
-    <div className="relative w-full max-w-80xl aspect-video overflow-hidden">
+    <div className="card relative w-full overflow-hidden">
       <ConfettiEffect />
       <CursorImageEffect />
       <RandomImages />
-      {/* Placeholder for background image */}
-      {/* <div className="absolute inset-0 bg-gray-200 flex items-center justify-center">
-        <span className="text-gray-500">Add your background image here</span>
-      </div> */}
-      
-      <div className="relative z-20 h-full flex flex-col items-center justify-center p-4">
+      <div className="relative z-20 flex flex-col items-center justify-center p-4 h-full">
         <h1 className="text-6xl font-bold text-center mb-8 birthday-text">
           Happy Birthday
         </h1>
         <AudioPlayer />
       </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default BirthdayCard
 
